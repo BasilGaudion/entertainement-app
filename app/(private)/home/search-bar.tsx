822b9a0e5301108e41df8736ebf320c76de947d7
BasilGaudion <basil.gaudion@yahoo.fr>
2024-01-29 17:09:07 +0100

    Update search bar and trending carousel

--- app/(private)/home/search-bar.tsx
+++ app/(private)/home/search-bar.tsx
@@ -4,7 +4,7 @@ import { Input } from "@/components/ui/input";
 
 const SearchBar = () => {
   return (
-    <div className="mt-14 pr-9 flex items-center">
+    <div className="mt-14 pr-9 flex items-center max-w-screen-md">
       <Image src={SearchIcon} alt="search" width={32} height={32} />
       <Input type="text" placeholder="Search for movies or TV series" />
     </div>
