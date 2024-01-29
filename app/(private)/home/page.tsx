a6ea86d836627e20493a7a84e09ee5c6f46edad2
BasilGaudion <basil.gaudion@yahoo.fr>
2024-01-29 16:19:13 +0100

    Add trending movie thumbnail to home page

--- app/(private)/home/page.tsx
+++ app/(private)/home/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./search-bar";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import Image from "next/image";
 
 const Home = () => {
   return (
@@ -10,15 +11,22 @@ const Home = () => {
         <h1 className="text-4xl text-slate-50 font-body font-normal">
           Trending
         </h1>
-        <div>
-          <div className="w-[470px] h-[230px]">
-            <Card className="h-full flex flex-col justify-end pl-6">
+        <div className="w-[470px] h-[229px] mt-6">
+          <div className="w-[470px] h-[229px] relative">
+            <Image
+              src="/assets/thumbnails/1998/trending/large.jpg"
+              alt="interstellar"
+              width={470}
+              height={230}
+              className="rounded-lg absolute z-0 top-0 left-0"
+            />
+            <Card className="h-full flex flex-col justify-end pl-6 z-10">
               <CardContent>
                 <h3 className="font-body text-white opacity-75 text-sm">
                   2019 &middot; Movie &middot; PG{" "}
                 </h3>
               </CardContent>
-              <CardFooter className="pb-6">
+              <CardFooter className="pb-6 z-10">
                 <h2 className="font-body text-2xl text-white">Beyond Earth</h2>
               </CardFooter>
             </Card>
